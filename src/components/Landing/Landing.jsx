36f510dc7fc504981cf4f10d38955d28723e5410
Paul Vangelakos <paulvangelakos@gmail.com>
2019-02-05 12:16:58 -0700

    Adding fetch call for opening crawl theme

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import './Landing.scss';
 
 
 class Landing extends Component {
+  constructor() {
+    super();
+    this.state = {
+      openingCrawl: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://swapi.co/api/films/1/')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          openingCrawl: data.opening_crawl
+        });
+      })
+      .catch(error => console.log(error));
+    }
+
   render() {
     return (
       <div className='Landing'>
-        <p>Landing Page</p>
+        <p className="opening-crawl">{this.state.openingCrawl}</p>
       </div>
     );
   }
